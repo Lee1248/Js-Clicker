19ae4bec84f987d0acf2664ef30db42041ec3692
Lee1248 <105126590+Lee1248@users.noreply.github.com>
2023-11-03 23:45:43 +0100

    The javascripts is now in its own file
    migrated the js code from the html file to the clicker.js file and linked it to the html

--- Clicker.js
+++ Clicker.js
@@ -1,1 +1,67 @@
-document.getElementById('demo').innerHTML = 'Hello JavaScript';
+ 
+  
+    var num = 0;
+    var _upg = false;
+    var _clk = false;
+    var spau = 0;
+    var _sp = false;
+  
+  
+function myFunction() {
+  num++;
+  if(_upg == true) {
+    num++;
+  }
+  if(_sp == true) {
+    num = num + 4;
+  }
+  document.getElementById("btn").innerHTML = num;
+}
+  
+  
+  function atclk() {
+    num++;
+    document.getElementById("btn").innerHTML = num;
+  }
+  
+  
+function upgrade() {
+  if(_upg == false && num >= 100) {
+  _upg = true;
+  num = num - 100;
+    document.getElementById("btn").innerHTML = num;
+    document.getElementById("upg").innerHTML = "Bought";
+  }
+  
+}
+  
+  
+  function upgrade2() {
+    if(num >= 400) {
+      _clk = true;
+      num = num - 400;
+      setInterval(atclk, 1000);
+    document.getElementById("btn").innerHTML = num;
+    }
+  }
+  
+  
+  function upgrade3() {
+    if(num >= 1400) {
+      _clk2 = true;
+      num = num - 1400;
+      setInterval(atclk, 250);
+      spau++;
+    document.getElementById("btn").innerHTML = num;
+    }
+  }
+  
+  
+  function multiAndAutoclk() {
+    if(_sp == false && spau >= 10) {
+      _sp = true;
+      setInterval(atclk, 50);
+    }
+  }
+  
+  
