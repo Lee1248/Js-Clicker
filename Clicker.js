44d65030aa08c6927e98c1112faf297213789586
Lee1248 <105126590+Lee1248@users.noreply.github.com>
2023-12-15 14:19:31 +0100

    QoL and Improvements
    G'day. Long time no see! Anyways, here are the changes:
    -added stats
    +times clicked
    +times upgraded
    -fixed a bug where it wouldnt continue to show "Done"  if you finished the mission/quest

--- Clicker.js
+++ Clicker.js
@@ -1,5 +1,6 @@
     var num = 0;
     var _upg = false;
+    let _upgCounter = 0;
     var _clk = false;
     var spau = 0;
     var _sp = false;
@@ -7,6 +8,8 @@
 function myFunction() {
   num++;
     m1++;
+    m2++;
+    document.getElementById("_s1").innerHTML ="Clicked times: " + m1;
   if(_upg == true) {
     num++;
   }
@@ -27,9 +30,11 @@ function myFunction() {
 function upgrade() {
   if(_upg == false && num >= 100) {
   _upg = true;
+      _upgCounter++;
   num = num - 100;
     document.getElementById("btn").innerHTML = num;
     document.getElementById("upg").innerHTML = "Bought";
+      document.getElementById("_s2").innerHTML = "Upgraded times: " + _upgCounter;
   }
   
 }
@@ -38,9 +43,11 @@ function upgrade() {
   function upgrade2() {
     if(num >= 400) {
       _clk = true;
+        _upgCounter++;
       num = num - 400;
       setInterval(atclk, 1000);
     document.getElementById("btn").innerHTML = num;
+        document.getElementById("_s2").innerHTML = "Upgraded times: " + _upgCounter;
     }
   }
   
@@ -48,10 +55,12 @@ function upgrade() {
   function upgrade3() {
     if(num >= 1400) {
       _clk2 = true;
+        _upgCounter++;
       num = num - 1400;
       setInterval(atclk, 250);
       spau++;
     document.getElementById("btn").innerHTML = num;
+        document.getElementById("_s2").innerHTML = "Upgraded times: " + _upgCounter;
     }
   }
   
@@ -59,7 +68,9 @@ function upgrade() {
   function multiAndAutoclk() {
     if(_sp == false && spau >= 10) {
       _sp = true;
+        _upgCounter++;
       setInterval(atclk, 50);
         document.getElementById("upgUt").innerHTML = "Bought";
+        document.getElementById("_s2").innerHTML = "Upgraded times: " + _upgCounter;
     }
   }
--- Missions.js
+++ Missions.js
@@ -1,10 +1,22 @@
 var m1 = 0;
+var m2 = 0;
 var mCoins = 0;
 function missions() {
     document.getElementById("_m1").innerHTML = "-Click 100 times: " + m1;
-    if(m1 == 100) {
+    document.getElementById("_m2").innerHTML = "-Click 1k times : " + m2;
+    if(m1 > 99) {
         document.getElementById("_m1").innerHTML = "-Click 100 times: Done";
+    }
+    if(m2 > 999) {
+        document.getElementById("_m2").innerHTML = "-Click 1000 times: Done";
+    }
+    if(m1 == 100) {
         mCoins = mCoins + 10;
         document.getElementById("coins").innerHTML = mCoins;
     }
+    if(m2 == 1000) {
+        mCoins = mCoins + 90;
+        document.getElementById("coins").innerHTML = mCoins;
+    }
+    
 }
